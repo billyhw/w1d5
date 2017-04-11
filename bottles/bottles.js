5bb857ae484e258e6480d898bff9c30780f7c293
Billy Chang <billyheungwing@yahoo.ca>
2017-04-11 00:14:39 +0000

    added bottles.js

--- bottles/bottles.js
+++ bottles/bottles.js
@@ -6,6 +6,10 @@ var newAccount = function(investment) {
 
   this.totalBottles = 0;
 
+  this.totalBottlesFromEmptyBottles = 0;
+
+  this.totalBottlesFromCaps = 0;
+
   this.numEmptyBottles = 0;
 
   this.numCaps = 0;
@@ -25,29 +29,39 @@ var newAccount = function(investment) {
   this.addBottlesFromEmptyBottles = function () {
     this.numBottles += Math.floor(this.numEmptyBottles / 2);
     this.totalBottles += Math.floor(this.numEmptyBottles / 2);
+    this.totalBottlesFromEmptyBottles += Math.floor(this.numEmptyBottles / 2);
     this.numEmptyBottles -= 2 * Math.floor(this.numEmptyBottles / 2);
   }
 
   this.addBottlesFromCaps = function () {
     this.numBottles += Math.floor(this.numCaps / 4);
     this.totalBottles += Math.floor(this.numCaps / 4);
+    this.totalBottlesFromCaps += Math.floor(this.numCaps / 4);
     this.numCaps -= 4 * Math.floor(this.numCaps / 4);
   }
 
   this.getMostBottles = function () {
     while (this.balance >= 2 || this.numEmptyBottles >= 2 || this.numCaps >= 4) {
       this.addBottlesFromBalance();
-      console.log("by balance:", this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
+      // console.log("by balance:", this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
       this.addBottlesFromCaps();
-      console.log("by Caps:", this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
+      // console.log("by Caps:", this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
       this.addBottlesFromEmptyBottles();
-      console.log("by Empty Bottles:",this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
+      // console.log("by Empty Bottles:",this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
       this.addEmptyBottlesAndCapsFromBottles();
-      console.log("add Empty Bottles:", this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
+      // console.log("add Empty Bottles:", this.balance, this.numBottles, this.numEmptyBottles, this.numCaps, this.totalBottles)
     }
+  }
+
+  this.informativePrint = function () {
+    console.log('From an investment of ', investment, 'dollars, you can...');
+    console.log('Obtain a total of ', this.totalBottles, 'bottles of pop.');
+    console.log("", this.totalBottlesFromEmptyBottles, " bottles are obtained from recycling empty bottles.")
+    console.log("", this.totalBottlesFromCaps, ' bottles are obtained from recycling caps.')
+    console.log('At the end, you will have ', this.numEmptyBottles, ' empty bottles and ', this.numCaps, ' caps left.')
   }
 }
 
-var account = new newAccount(6);
+var account = new newAccount(process.argv[2]);
 account.getMostBottles();
-account
+account.informativePrint();
